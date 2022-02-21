8740dea3cc9f6266a8f3d97e561cdc029bc369f0
Jonathan Potter <jonathan@mrjonathanpotter.com>
2022-02-21 12:18:01 -0800

    commit tree d145e2e726f21b31ce8945fb8f6d8e8ae522d0df

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,23 @@
+require 'pry'
 #write your code here
 
-def countdown
+def countdown(int)
+    while int >= 1
+        puts "#{int} SECOND(S)!"
+        int -= 1
+    end
+    return "HAPPY NEW YEAR!"
 end
+def countdown_with_sleep(int)
+    while int >= 1
+        puts "#{int} SECOND(S)!"
+        int -= 1
+        sleep(1)
+    end
+    return "HAPPY NEW YEAR!"
+end
+
+
+
+binding.pry
+0
